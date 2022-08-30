cb54a62db1072c6dbfaf0c9c167de1904d0a5da2
VirWus <walidvirwus@gmail.com>
2022-08-30 06:58:40 +0100

    yy

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
-
-import App from './App'
+import 'react-app-polyfill/ie11';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+//import * as serviceWorker from './serviceWorker';
+import { HashRouter } from 'react-router-dom'
+import ScrollToTop from './ScrollToTop';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+    <HashRouter>
+        <ScrollToTop>
+            <App></App>
+        </ScrollToTop>
+    </HashRouter>,
+    document.getElementById('root')
+);
